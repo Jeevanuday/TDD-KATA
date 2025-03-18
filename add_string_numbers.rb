c4465a974e3ec7fbc9c3e2d00f8d95be9c64037e
Jeevan U <jeevan.u@oracle.com>
2025-03-18 18:57:28 +0530

    Fixed variable names

--- add_string_numbers.rb
+++ add_string_numbers.rb
@@ -1,19 +1,20 @@
-def add_number(string)
-    return 0 if string.empty?
-    delimiter = ','
-    if string[0, 2] == "//"
-        delimiter = string[2]
-        string = string[4..-1]
-    end
-    new_line_check = string.include?("\n")
-    return 'Invalid input' if string.include?("#{delimiter}\n") || string.include?("\n#{delimiter}")
+def add_string_number(input_string)
+    return 0 if input_string.empty?
     
-    string = string.gsub("\n", delimiter) if new_line_check
-    numbers = string.split(delimiter).map(&:to_i)
-    negative_numbers = numbers.select { |num| num < 0 }
+    delimiter = ','
+    if input_string[0, 2] == "//"
+        delimiter = input_string[2]
+        input_string = input_string[4..-1]
+    end
+    new_line_check = input_string.include?("\n")
+    return 'Invalid input' if input_string.include?("#{delimiter}\n") || input_string.include?("\n#{delimiter}")
+    
+    input_string = input_string.gsub("\n", delimiter) if new_line_check
+    numbers_list = input_string.split(delimiter).map(&:to_i)
+    negative_numbers = numbers_list.select { |num| num < 0 }
     return "Negatives not allowed: #{negative_numbers.join(', ')}" if negative_numbers.any?
 
-    remove_big_numbers = numbers.select { |num| num > 1000 }
-    numbers = numbers - remove_big_numbers
-    numbers.reduce(:+)
+    remove_big_numbers = numbers_list.select { |num| num > 1000 }
+    numbers_list = numbers_list - remove_big_numbers
+    numbers_list.reduce(:+)
 end
--- add_string_numbers_spec.rb
+++ add_string_numbers_spec.rb
@@ -2,72 +2,72 @@ require './add_string_numbers'
 
 describe 'add_string_numbers' do
     it 'return 0 when given an empty string' do
-        expect(add_number('')).to eq(0)
+        expect(add_string_number('')).to eq(0)
     end
 
     context 'When single number is passed' do
         it 'return 1 when given "1"' do
-            expect(add_number('1')).to eq(1)
+            expect(add_string_number('1')).to eq(1)
         end
         it 'return 2 when given "2"' do 
-            expect(add_number('2')).to eq(2)
+            expect(add_string_number('2')).to eq(2)
         end
     end
 
     context 'when multiple numbers are passed' do
         it 'return 3 when given "1,2"' do
-            expect(add_number('1,2')).to eq(3)
+            expect(add_string_number('1,2')).to eq(3)
         end
         it 'return 6 when given "1,2,3"' do
-            expect(add_number('1,2,3')).to eq(6)
+            expect(add_string_number('1,2,3')).to eq(6)
         end
     end
 
     context 'when new line is passed' do
         it 'return 3 when given "1\n2"' do
-            expect(add_number("1\n2")).to eq(3)
+            expect(add_string_number("1\n2")).to eq(3)
         end
         it 'return 6 when given "1\n2\n3"' do
-            expect(add_number("1\n2\n3")).to eq(6)
+            expect(add_string_number("1\n2\n3")).to eq(6)
         end
         it 'return error when given "1,\n"' do
-            expect(add_number("1,\n")).to eq('Invalid input')
+            expect(add_string_number("1,\n")).to eq('Invalid input')
         end
     end
 
     context 'when custom delimiter is passed' do
         it 'return 3 when given "//;\n1;2"' do
-            expect(add_number("//;\n1;2")).to eq(3)
+            expect(add_string_number("//;\n1;2")).to eq(3)
         end
         it 'return 6 when given "//;\n1;2;3"' do
-            expect(add_number("//;\n1;2;3")).to eq(6)
+            expect(add_string_number("//;\n1;2;3")).to eq(6)
         end
         it 'return error when given "//;\n1;\n"' do       
-            expect(add_number("//;\n1;\n")).to eq('Invalid input')
+            expect(add_string_number("//;\n1;\n")).to eq('Invalid input')
         end
     end
 
     context 'when negative numbers are passed' do
         it 'return error when given "-1"' do
-            expect(add_number('-1')).to eq('Negatives not allowed: -1')
+            expect(add_string_number('-1')).to eq('Negatives not allowed: -1')
         end
         it 'return error when given "-1,-2"' do
-            expect(add_number('-1,-2')).to eq('Negatives not allowed: -1, -2')
+            expect(add_string_number('-1,-2')).to eq('Negatives not allowed: -1, -2')
         end
         it 'return error when given "//;\n-1;2;-3"' do
-            expect(add_number("//;\n-1;2;-3")).to eq('Negatives not allowed: -1, -3')
+            expect(add_string_number("//;\n-1;2;-3")).to eq('Negatives not allowed: -1, -3')
         end
     end
 
     context 'when number is greater than 1000' do     
         it 'return 2 when given "2,1001"' do
-            expect(add_number('2,1001')).to eq(2)
+            expect(add_string_number('2,1001')).to eq(2)
         end
         it 'return 3 when given "1,2,1001"' do
-            expect(add_number('1,2,1001')).to eq(3)
+            expect(add_string_number('1,2,1001')).to eq(3)
         end
         it 'return 6 when given "1,2,1001,3"' do
-            expect(add_number('1,2,1001,3')).to eq(6)
+            expect(add_string_number('1,2,1001,3')).to eq(6)
         end
     end
 end
