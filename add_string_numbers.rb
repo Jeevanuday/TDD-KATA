5582ab18b6830cc870b2584c83f3e4ce56941c1e
Jeevan U <jeevan.u@oracle.com>
2025-03-18 19:22:27 +0530

    Modified code to reduce the lines

--- add_string_numbers.rb
+++ add_string_numbers.rb
@@ -1,20 +1,20 @@
 def add_string_number(input_string)
     return 0 if input_string.empty?
-    
-    delimiter = ','
-    if input_string[0, 2] == "//"
-        delimiter = input_string[2]
-        input_string = input_string[4..-1]
-    end
-    new_line_check = input_string.include?("\n")
+
+    delimiter, input_string = fetch_delimeter(input_string)
     return 'Invalid input' if input_string.include?("#{delimiter}\n") || input_string.include?("\n#{delimiter}")
     
-    input_string = input_string.gsub("\n", delimiter) if new_line_check
+    input_string.gsub!("\n", delimiter)
     numbers_list = input_string.split(delimiter).map(&:to_i)
+
     negative_numbers = numbers_list.select { |num| num < 0 }
     return "Negatives not allowed: #{negative_numbers.join(', ')}" if negative_numbers.any?
 
-    remove_big_numbers = numbers_list.select { |num| num > 1000 }
-    numbers_list = numbers_list - remove_big_numbers
+    numbers_list.reject! { |num| num > 1000 }
     numbers_list.reduce(:+)
+end
+
+def fetch_delimeter(input_string)
+    return [input_string[2], input_string[4..-1]] if input_string[0, 2] == "//"
+    [',', input_string]
 end
